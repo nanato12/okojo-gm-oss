08648e0392b976ce67335509a8b2013da3067ea9
nanato12 <admin@nanato12.info>
2021-03-14 23:09:49 +0900

    Add LineBotController ・Messaging API のwebhook処理の追加

---
+++ laravel/app/Http/Controllers/LineBotController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\Exception\InvalidEventRequestException;
+use LINE\LINEBot\Exception\InvalidSignatureException;
+use OkojoBot\OkojoBot;
+
+class LineBotController extends Controller
+{
+    /**
+     * LINEBot webhook
+     *
+     * @param Request $request HTTPリクエスト
+     *
+     * @return Response レスポンス
+     */
+    public function callback(Request $request)
+    {
+        /** @var LINEBot $bot */
+        $bot = app('line-bot');
+
+        // LINEのシグネチャ確認
+        if (isset($_SERVER['HTTP_' . HTTPHeader::LINE_SIGNATURE])) {
+            $signature = $_SERVER['HTTP_' . HTTPHeader::LINE_SIGNATURE];
+            if (empty($signature)) {
+                return Response('Bad Request', 400);
+            }
+        } else {
+            return Response('Bad Request', 400);
+        }
+
+        // イベントパース
+        try {
+            $events = $bot->parseEventRequest($request->getContent(), $signature);
+        } catch (InvalidSignatureException $e) {
+            return Response('Invalid signature', 400);
+        } catch (InvalidEventRequestException $e) {
+            return Response('Invalid event request', 400);
+        }
+
+        // イベント処理
+        foreach ($events as $event) {
+            if (env("APP_ENV") === "local") {
+                error_log($request->getContent());
+            }
+            $okojoBot = new OkojoBot($bot, $event);
+            $okojoBot->do();
+        }
+
+        return Response('OK', 200);
+    }
+}
