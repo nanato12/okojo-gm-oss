f99aeb93df513ddd2534dfcf7f79f4094bbdcf8a
nanato12 <admin@nanato12.info>
2021-03-14 23:19:13 +0900

    Update web.php ・callbackエンドポイントの追加

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/callback', 'LineBotController@callback')->name("linebot.callback");
