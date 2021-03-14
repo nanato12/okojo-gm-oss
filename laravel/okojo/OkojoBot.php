ca58a5b978c156f6f0b7c37a21db0aa1fc0d161a
nanato12 <admin@nanato12.info>
2021-03-15 07:29:46 +0900

    Update OkojoBot ・Phineの廃止

--- laravel/okojo/OkojoBot.php
+++ laravel/okojo/OkojoBot.php
@@ -2,24 +2,24 @@
 
 namespace OkojoBot;
 
+use LINE\LINEBot;
 use LINE\LINEBot\Event\BaseEvent;
 use LINE\LINEBot\Event\MessageEvent;
 use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use OkojoBot\MessageReciever\BaseReciever;
 use OkojoBot\MessageReciever\TextMessageReciever;
-use Phine\Client;
 
 interface IOkojoBot
 {
     /**
      * 各イベントタイプごとにレシーバを設定する。
      *
-     * @param Client $bot Botインスタンス
+     * @param LINEBot $bot Botインスタンス
      * @param BaseEvent $event イベントオブジェクト
      *
      * @return void
      */
-    function setReciever(Client $bot, BaseEvent $event): void;
+    function setReciever(LINEBot $bot, BaseEvent $event): void;
 
     /**
      * レシーバを実行する。
@@ -35,22 +35,19 @@ class OkojoBot implements IOkojoBot
     private $reciever;
 
     /**
-     * Clientの処理をする。
+     * LINEBotの処理をする。
      *
-     * @param Client $bot Botインスタンス
+     * @param LINEBot $bot Botインスタンス
      * @param BaseEvent $event イベントオブジェクト
      */
-    function __construct(Client $bot, BaseEvent $event)
+    function __construct(LINEBot $bot, BaseEvent $event)
     {
         // レシーバの設定
         $this->setReciever($bot, $event);
     }
 
-    function setReciever(Client $bot, BaseEvent $event): void
+    function setReciever(LINEBot $bot, BaseEvent $event): void
     {
-        // リプライトークンをセットする。
-        $bot->setReplyToken($event->getReplyToken());
-
         // メッセージイベント
         if ($event instanceof MessageEvent) {
             // テキストメッセージ
