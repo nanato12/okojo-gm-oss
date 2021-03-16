7881fd02b538559703b15b87450c4cfbdcae4460
nanato12 <admin@nanato12.info>
2021-03-16 10:43:39 +0900

    Update ProfileController ・register関数の追加 ・isRegistered関数の追加

--- laravel/okojo/Controllers/ProfileController.php
+++ laravel/okojo/Controllers/ProfileController.php
@@ -5,13 +5,28 @@ namespace OkojoBot\Controllers;
 use App\Models\Profile;
 use App\Models\RPG;
 
+interface IProfileController
+{
+    /**
+     * 新規登録する関数
+     */
+    function register(): void;
+
+    /**
+     * 新規登録済か判定する関数
+     *
+     * @return bool
+     */
+    function isRegistered(): bool;
+}
+
 /**
  * ユーザープロフィールを管理するコントローラ
  *
  * @property string $uid ユーザーID
  * @property Profile $profile Profileモデル
  */
-class ProfileController
+class ProfileController implements IProfileController
 {
     /**
      * @var string $uid ユーザーID
@@ -26,6 +41,10 @@ class ProfileController
     function __construct(string $uid)
     {
         $this->uid = $uid;
+
+        /**
+         * @var null|Profile　$profile Profileモデル
+         */
         $profile = Profile::where('uid', $uid)->first();
         if (is_null($profile)) {
             $profile = new Profile;
@@ -36,5 +55,16 @@ class ProfileController
             );
         }
         $this->profile = $profile;
+    }
+
+    function register(): void
+    {
+        $this->profile->is_registered = true;
+        $this->profile->save();
+    }
+
+    function isRegistered(): bool
+    {
+        return $this->profile->is_registered;
     }
 }
