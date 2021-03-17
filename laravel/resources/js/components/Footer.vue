869aeec9eecd3caea3b1a03926323fcb08ce08bb
nanato12 <admin@nanato12.info>
2021-03-17 11:26:52 +0900

    Add Footer.vue ・フッターの追加

---
+++ laravel/resources/js/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-footer dark absolute padless>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} — <strong>nanato12 - OkojoGM</strong>
+    </v-col>
+  </v-footer>
+</template>
