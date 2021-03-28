7cb97190a0808e28338d5d197c2a3ad1b586349a
nanato12 <admin@nanato12.info>
2021-03-29 02:13:24 +0900

    Update Profile ・招待コードからUIDを検索する関数の追加

--- laravel/app/Models/Profile.php
+++ laravel/app/Models/Profile.php
@@ -20,4 +20,18 @@ class Profile extends Model
     {
         return $this->hasOne(RPG::class, 'uid', 'uid');
     }
+
+    /**
+     * 招待コードからユーザーIDを取得する関数
+     *
+     * @return string|null ユーザーID
+     */
+    public static function getUidByInviteCode(string $inviteCode): ?string
+    {
+        $profile = self::where('invite_code', $inviteCode)->first();
+        if (is_null($profile)) {
+            return null;
+        }
+        return $profile->uid;
+    }
 }
